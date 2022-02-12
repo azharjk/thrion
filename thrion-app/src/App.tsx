88365e59c9d978e6ddbe943d5498fda7b1a9b182
azharjk <azhr.project@gmail.com>
2022-02-12 21:27:29 +0700

    Add header

--- thrion-app/src/App.tsx
+++ thrion-app/src/App.tsx
@@ -8,7 +8,43 @@ import Register from "./pages/Register";
 const App = () => {
   return (
     <>
-      <header></header>
+      <header>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="search-icon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
+        <div className="menu">
+          <h1 className="menu__title">ThriØn</h1>
+          <nav>
+            <ul className="menu__links">
+              <li>
+                <a href="">Women's Wear</a>
+              </li>
+              <li>
+                <a href="">Newcomers</a>
+              </li>
+              <li>
+                <a href="">Men's Wear</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <a className="login-link" href="">Login</a>
+        </div>
+      </header>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
