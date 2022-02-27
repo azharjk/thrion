b202bab346cfa078d0c812172a11372f2baea887
unknown <farisahmad1210@gmail.com>
2022-02-27 17:00:45 +0700

    menembahakan page login

--- thrion-app/src/pages/Login.tsx
+++ thrion-app/src/pages/Login.tsx
@@ -3,13 +3,12 @@ import React from "react";
 const Login = () => {
   return <>
   <div className="log">
-  <h1 className="log-in">LOGIN</h1>
   <form>
-  <label>Username</label><br/>
+  <label className="log-in">Username</label><br/>
                 <input type="text" /><br/>
                 <label>Password</label><br/>
                 <input type="password" /><br/>
-                <button>Log In</button>
+                <button>Login</button>
   </form>
   </div>
   </>
